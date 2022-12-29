e74a491e55b9ec62a714f80c6ecd8a531f85dd45
nic <nrhoda14@gmail.com>
2022-12-28 22:58:41 -0600

    defined columns in product.js, category.js, tag.js, and producttag.js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,13 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // continue later
   },
   {
     sequelize,
