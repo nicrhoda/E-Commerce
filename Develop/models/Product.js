8843b20744552cb5a57ca9a4259c6cd1d4e9f848
nic <nrhoda14@gmail.com>
2023-01-02 14:50:10 -0600

    completed productTag.js and product.js models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -18,6 +18,24 @@ Product.init(
     product_name: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      isNumeric: true
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      isNumeric: true
     }
   },
   {
